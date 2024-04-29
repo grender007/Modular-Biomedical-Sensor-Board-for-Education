70b50d6acce51bf23d99bc66e277e2ab699b68a4
grender007 <112654671+grender007@users.noreply.github.com>
2024-04-29 01:38:49 -0700

    Update Filter_1.m

--- Sound Sensor/Arduino Code/Filter_1.m
+++ Sound Sensor/Arduino Code/Filter_1.m
@@ -2,12 +2,12 @@ fc_l = 1150;    % Cutoff frequency for LPF
 fc_h = 100;      % Cutoff frequency for HPF
 fs = 8000;      % Sampling Frequency
 
-% 2nd order LPF 
+% 2nd order LPF. To change the order of the filter, modify the first parameter in "butter(2,fc_l/(fs/2))"
 [b_l,a_l] = butter(2,fc_l/(fs/2));
 %freqz(b_l,a_l,[],fs);
 %subplot(2,1,1);
 
-% 2nd order HPF 
+% 2nd order HPF. To change the order of the filter, modify the first parameter in "butter(2,fc_l/(fs/2), 'high')"
 [b_h,a_h] = butter(2,fc_h/(fs/2), 'high');
 %freqz(b_h,a_h,[],fs);
 %subplot(2,1,1);
